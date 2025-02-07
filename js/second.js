ce494c3e63c2d9110b27f3166cd00bdb777c64d0
Aaryashkc <aaryashkc123@gmail.com>
2025-02-07 23:09:51 +0545

    final push

--- js/second.js
+++ js/second.js
@@ -84,7 +84,7 @@ function calculateCompatibility() {
     const textElement = document.querySelector('#card4 .card-text');
     
     // Change the image
-    imageElement.src = "/valentine/assets/love.png";  // Make sure you have this image in your assets folder
+    imageElement.src ='../assets/love.png';
     
     // Change the title to h6 and update text
     titleElement.outerHTML = '<h6 style="font-size: 1.2rem;" class="card-title">See? Told you! 😏</h6>';
